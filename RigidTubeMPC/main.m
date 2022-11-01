a9ef1bc8ecfcc43b38ff55abc4a77bcd06dc3c60
sohail70 <sohail.e.nia@gmail.com>
2022-11-01 23:35:16 +0330

    Add main.m without any content

---
+++ RigidTubeMPC/main.m
@@ -0,0 +1,12 @@
+%length of simulation
+
+% generate disturbances for the process --> for example some random w inside W
+
+%pic up and initial condition
+
+% Executing robust tube model predictive control rtmpc
+
+
+
+
+%% plot the controllable sets and state tubes control tubes and plot the cost
