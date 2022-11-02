22c498fd575ef358a1a2267b3a0ba6e935cdfa07
Senia <sohail.e.nia@gmail.com>
2022-11-02 14:34:00 +0330

    Add simple online control

--- RigidTubeMPC/data.m
+++ RigidTubeMPC/data.m
@@ -34,10 +34,22 @@ constraints.d = 2;
 
 constraints.D = [1;-1]; 
 constraints.e = [1;1];
-N=9;
+N=20;
 %% cost
 cost.Q = (0.5)*eye(2); %albate dar lecture note (0.5) nadare vali dar paper az 0.5 use kardim
 cost.R = (0.5)*0.01;
+
+%dar moreded rabeteye paeen --> Khodam : avalan ke dlqr estefade kardim
+%chun dynnamics e ma double integrator dar halate discrete time hast ke ba
+%continuous equation esh fargh mikune. dovoman khoroji in func e paeen
+%mishe k vase u=-kx ke age equation ro benvisi mishe xdot = (A-BK)x ke age
+%ye halghe for bezari va x(k+1) = (A-BK)*x(k) ro use kuni va initial
+%conidtion ro har jaee bedi mibini ke bad az chand iteration in control to
+%ro mibare be origin va onja stabilize eet mikune yani iteration ha
+%bishatar ham beshe baz to origin mimone x et. sevoman problem ma alan w
+%dare ke dlqr handlesh nemikune va ma faghat az in darim use mikunim baraye
+% faghat bakhsh e nominal emoon yani z ke disturbance nadare . chaharoman
+% ma to jozve az u=kx use kardim pas k e ke dlqr mide ro makoos mikunim
 [system.K,designIngrediant.P] = dlqr(system.A,system.B,cost.Q,cost.R); %albate in K optimal vase u = -Kx ro mide ama dara jozve ma az Kx be onvane control estedafe kardim pas K=-K bezar badesh
 system.K = -system.K; % choon to jozve azu=Kx estefade kardim vali matlab func e dlqr u=-kx ro use mikune
 %designIngrediant.P ke dar miari mishe nesfe oon chizi ke dar Paper
@@ -56,10 +68,36 @@ system.K = -system.K; % choon to jozve azu=Kx estefade kardim vali matlab func e
 
 % dar paper be set e shekl (oon polyhedra ha) mige z vali ma behesh goftim S
 
-% berim soraghe Set S
+% berim soraghe Set S --->Khodam: khob hala ke K e dlqr ro bedast avodrim system
+% dynamici jadid ro tashkil midim --> age w nadashte bashim ke hamonto ke
+% goftam kar rahate va stablize mishe be equlibrium point vali alan ke w
+% darim kar sakhtare pas miaym dore on state ye region ee ro moshakhas
+% mikunim ke agar oon to bashim no matter what the disturbance is betonim
+% stablize kunim khodemono ba in u ee ke lqr dar ekhtiare ma gozashte
 [designIngrediant.S,~] = RPIapproxOfMinimal(system,W,0.1); % so for the MPC we use Outer tube not inner tube
 %alan age plot(designIngrediant.S) kuni mibini shabih shekl paper mishe dar Fig.2
 %deghat kun in ye approximation hast
+
+% khodam ---> in shekl mishe MinPIS yani dynamic e bad az emale K ro 
+% yani Xdot = (A+BK)X ro age dar nazar begiri har noghtee e age oon to
+% bashe dg hamoon to mimune ! ye test ee bokonim bebinim dorost hast ya na
+% deghat kun noghte avalie ro dakhele designIngrediant.S bayad begiri
+
+%{
+position = designIngrediant.S.randomPoint;
+position = [-0.25;0.25] ; %check kun --> designIngrediant.S.contains([-0.2;0.2])
+plot(designIngrediant.S);
+hold on;
+p  = plot(position(1),position(2),'ob');
+while(true)
+%     delete(p);
+    position = (system.A+system.B*system.K)*position + W.randomPoint ; %khob alan age W.randomPoint nabashe ke lqr miad position ro mibare be origin vali age disturbance ham bashe lqr mitoone zemanat kune ke toye nahyeye MinRPIS mimune ke mitoni to in simulation ham bebini
+    p = plot(position(1),position(2),'ob');
+    drawnow;
+end
+%}
+
+
 
 %constraints in a polyhedral form
 X = Polyhedron(constraints.C,constraints.d);
@@ -133,3 +171,29 @@ designIngrediant.Zf = X(di); % The last one is Zf
 [H,f] = Cost(cost,designIngrediant.P,N);
 [A_in,c_in,C_in] = InequalityConstraints(designIngrediant,N,size(cost.Q,1),size(cost.R,1));
 [A_eq,b_eq] = EqualityConstraints(system,N,size(cost.Q,1),size(cost.R,1)); 
+
+n = size(cost.Q,1);
+m = size(cost.R,1);
+x = [15;-15];
+plot(x(1),x(2),'or');
+hold on;
+sim_time = 100;
+% agar optimization infeasible bood N ro ziad kun
+for i = 1:sim_time
+    b_in = c_in + C_in*x;
+    sol = quadprog(0.5*H,f,A_in,b_in,A_eq,b_eq);
+    x_nominal_seq = reshape(sol(1:(N+1)*n) , n, N+1);
+    u_nominal_seq = reshape(sol((N+1)*n+1:end) , m, N);
+    
+    u_nominal = u_nominal_seq(:,1);
+    
+    u_feedback = system.K*(x - x_nominal_seq(:,1));
+    u_next = u_nominal + u_feedback;
+    
+    random_w = W.randomPoint;
+    
+    x = system.A * x + system.B * u_next + random_w;
+    plot(x(1),x(2),'or');
+    hold on;
+    pause(0.5);
+end
